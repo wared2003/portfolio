95a5a43448c3974f32dc2260b0f01175cfcbcf43
Edouard <edouard.nicolas@edu.devinci.fr>
2021-11-01 22:48:14 +0100

    [update]fix-bug

---
+++ js/main.js
@@ -0,0 +1,28 @@
+window.onload = function () {
+    console.log(navigator.appName)
+    curent_page_dom = document.getElementsByTagName("body")[0];
+    curent_page = curent_page_dom.getAttribute('data-curentPage');
+    var scroll_y = window.scrollY;
+    var body_height = document.querySelector("body").getBoundingClientRect().height;
+    document.querySelector("body").style.height = body_height - scroll_y * 4 + "px";
+    console.log(body_height);
+    document.querySelector("html").style.height = body_height + "px !important";
+
+
+    if (curent_page == "index") {
+        window.addEventListener('scroll', () => {
+            let section_competences = document.querySelector('#section_competences');
+            var scroll_y = window.scrollY;
+            section_competences.style.bottom = scroll_y * 0.4 + "px";
+            let section_project_cards = document.querySelector('#projects-cards');
+            section_project_cards.style.bottom = scroll_y * 0.9 + "px";
+            let footer = document.querySelector('#footer');
+            footer.style.bottom = scroll_y * 0.9 + "px";
+            var body_height = document.querySelector("body").getBoundingClientRect().height;
+            document.querySelector("body").style.height = body_height - scroll_y * 4 + "px";
+            console.log(body_height);
+            document.querySelector("html").style.height = body_height + "px";
+        })
+
+    }
+}
